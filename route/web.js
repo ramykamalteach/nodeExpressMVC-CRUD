702b21bca062bd082ef588dcb06401f4e05f8da1
ramy.kamal.teachx <76243913+ramyyat@users.noreply.github.com>
2024-01-29 18:25:16 +0200

    prepare project

--- route/web.js
+++ route/web.js
@@ -2,36 +2,12 @@ const express = require('express');
 const webRouter = express.Router();
 
 /* ------------- controllers ------------------- */
-const staffController = require('../controllers/staffController');
-const {dishController} = require('../controllers/dishController');
 
 const productController = require('../controllers/productController');
 
 /* -------------- route roles ------------------------ */
-webRouter.get('/', (req, res) => {
-    res.render('pages/home');
-});
-webRouter.get('/about', (req, res) => {
-    res.render('pages/about');
-});
-webRouter.get('/contacts', (req, res) => {
-    res.render('pages/contacts');
-});
 
-webRouter.get('/staff', (req, res) => {
-    webRouter.get('/staff', staffController(req, res));
-});
-
-
-webRouter.get('/dishes', async (req, res) => {
-    try {
-        await dishController(req, res);
-    }
-    catch (error) {
-        throw error;
-    }
-});
-/* ------------------------------------------ */
+/* -------------------products route roles----------------------- */
 webRouter.get('/products', (req, res) => {
     productController.index(req, res);
 });
